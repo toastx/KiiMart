3a02a318089f9cfb3a53b20aeca76bc76b18fdd5
Toastx <toastx.sol@gmail.com>
2024-09-05 00:48:22 +0530

    i messed up

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,7 +7,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let contract = contracts::load_contract().await?;
     let buyer_address = address!("104dc4c1FcA6359B9bdBf81705E34f1ba91a3958");
     let seller = address!("F890F95047D40e59c42a3E6d5720a89EE29453cE");
-    let seller_private_key = contracts::generate_private_key("3f7e255c7a960413344eb493980a17696d97f94285b443891184aa15d767a04d");
+    let private_key = std::env::var("private_key").expect("private_key must be set.");
+    let seller_private_key = contracts::generate_private_key(&private_key);
     let wallet = contracts::generate_wallet_obj(seller_private_key);
     let buyer_amount = 220;
     let seller_amount = 200;
