243bce1e0d101e7e1e3f12afc4b717c51d5f0a62
Toastx <toastx.sol@gmail.com>
2024-09-05 00:44:15 +0530

    txn finally working- createEscrow done

--- backend/src/contracts/contracts.rs
+++ backend/src/contracts/contracts.rs
@@ -78,20 +78,7 @@ pub fn generate_wallet_obj(key:LocalSigner<SigningKey>) -> EthereumWallet {
     wallet
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Report> {
-    let contract = load_contract().await?;
-    let buyer_address = address!("104dc4c1FcA6359B9bdBf81705E34f1ba91a3958");
-    let seller = address!("F890F95047D40e59c42a3E6d5720a89EE29453cE");
-    let seller_private_key = PrivateKeySigner::from_str("3f7e255c7a960413344eb493980a17696d97f94285b443891184aa15d767a04d").unwrap();
-    let wallet = EthereumWallet::from(seller_private_key);
-    let buyer_amount = 220;
-    let seller_amount = 200;
-    let order_id = "test".to_string();
-    
-    create_escrow(&contract, buyer_address, seller, buyer_amount, seller_amount, order_id,wallet).await?;
-    Ok(())
-}
+
 
 
 
